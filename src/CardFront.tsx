37999931904a24b098755b6f0f33d4c2a0b1ae51
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-13 15:08:51 -0400

    create card component

---
+++ src/CardFront.tsx
@@ -0,0 +1,9 @@
+import cardFrontImage from "./images/bg-card-front.png"
+
+export const CardFront = () => {
+  return (
+    <div>
+      <img src={cardFrontImage} alt="card-front"/>
+    </div>
+  );
+}
