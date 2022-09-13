09e2599e3882199ce529ddcc0fc60f237eed2e79
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-13 14:50:45 -0400

    feat: background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     interactive card
+      <div className="container-left">
+      </div>
+      <div className="container-right"></div>
     </div>
   );
 }
