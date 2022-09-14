11a7b65f6acee96b12a55635969d2b7bbeac967e
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-14 08:11:14 -0400

    onChange events for name and card number

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from "react";
 import "./App.css";
 import { CardFront } from "./components/CardFront";
+import { useState } from "react";
 
 function App() {
+  const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000");
+  const [cardHolder, setCardHolder] = useState("Jane Appleseed");
+  const [cardExpDate, setCardExpDate] = useState("00/00");
+
+  const handleCardNumberChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCardNumber(event.target.value);
+  };
+
+  const handleCardHolderChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCardHolder(event.target.value);
+  };
+
   return (
     <div className="App">
-        <div className="card-front-container">
-          <CardFront />
-        </div>
+      <div className="card-front-container">
+        <CardFront
+          cardNumber={cardNumber}
+          cardHolder={cardHolder}
+          cardExpDate={cardExpDate}
+        />
+      </div>
       <div className="container-left"></div>
       <div className="container-right">
         <form>
           <p className="input-title">Cardholder Name</p>
-          <input type="text" placeholder="e.g. Jane Appleseed" />
+          <input
+            className="input-box"
+            type="text"
+            placeholder="e.g. Jane Appleseed"
+            onChange={(e) => handleCardHolderChange(e)}
+          />
           <p className="input-title">Card Number</p>
-          <input type="number" placeholder="e.g. 1234 5678 9012 3456" />
+          <input
+            className="input-box"
+            type="text"
+            placeholder="e.g. 1234 5678 9012 3456"
+            onChange={(e) => handleCardNumberChange(e)}
+          />
           <p className="input-title">Exp. Date (MM/YY)</p>
-          <input type="text" placeholder="MM" />
-          <input type="text" placeholder="YY" />
+          <input className="input-box" type="text" placeholder="MM" />
+          <input className="input-box" type="text" placeholder="YY" />
           <p className="input-title">CVC</p>
-          <input type="number" placeholder="e.g. 123" />
-          <button type="submit">Confirm</button>
+          <input className="input-box" type="number" placeholder="e.g. 123" />
+          <button className="confirm-button" type="submit">
+            Confirm
+          </button>
         </form>
       </div>
     </div>
--- src/components/CardFront/index.tsx
+++ src/components/CardFront/index.tsx
@@ -1,18 +1,26 @@
-import './CardFront.css';
-import cardFrontImage from "../../images/bg-card-front.png"
-import { useState } from 'react';
+import "./CardFront.css";
+import cardFrontImage from "../../images/bg-card-front.png";
+import { FC } from "react";
 
-export const CardFront = () => {
-  const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000");
-  const [cardHolder, setCardHolder] = useState("Jane Appleseed");
-  const [cardExpDate, setCardExpDate] = useState("00/00");
+export interface CardFrontProps {
+  cardNumber: string;
+  cardHolder: string;
+  cardExpDate: string;
+}
 
+export const CardFront: FC<CardFrontProps> = ({
+  cardNumber,
+  cardHolder,
+  cardExpDate,
+}: CardFrontProps): JSX.Element => {
   return (
-    <div className='card-front'>
-      <img src={cardFrontImage} alt="card-front"/>
-      <h1 className='card-front-number'>{cardNumber}</h1>
+    <div className="card-front">
+      <img src={cardFrontImage} alt="card-front" />
+      <div className="circle"></div>
+      <div className="open-circle"></div>
+      <h1 className="card-front-number">{cardNumber}</h1>
       <p className="card-front-name">{cardHolder}</p>
       <p className="card-front-date">{cardExpDate}</p>
     </div>
   );
-}
+};
