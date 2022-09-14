58f5d499e768f756c763dbedc2b7eaa8c0107abf
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-14 07:37:21 -0400

    created card info state

--- src/components/CardFront/index.tsx
+++ src/components/CardFront/index.tsx
@@ -1,13 +1,18 @@
 import './CardFront.css';
 import cardFrontImage from "../../images/bg-card-front.png"
+import { useState } from 'react';
 
 export const CardFront = () => {
+  const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000");
+  const [cardHolder, setCardHolder] = useState("Jane Appleseed");
+  const [cardExpDate, setCardExpDate] = useState("00/00");
+
   return (
     <div className='card-front'>
       <img src={cardFrontImage} alt="card-front"/>
-      <h1 className='card-front-number'>0000 0000 0000 0000</h1>
-      <p className="card-front-name">Jane Appleseed</p>
-      <p className="card-front-date">00/00</p>
+      <h1 className='card-front-number'>{cardNumber}</h1>
+      <p className="card-front-name">{cardHolder}</p>
+      <p className="card-front-date">{cardExpDate}</p>
     </div>
   );
 }
