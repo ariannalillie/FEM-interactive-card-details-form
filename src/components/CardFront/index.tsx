60a4b871f42fc0b66cd74419ccaf590236eac4b6
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-14 07:32:38 -0400

    name css on card front

--- src/components/CardFront/index.tsx
+++ src/components/CardFront/index.tsx
@@ -6,6 +6,8 @@ export const CardFront = () => {
     <div className='card-front'>
       <img src={cardFrontImage} alt="card-front"/>
       <h1 className='card-front-number'>0000 0000 0000 0000</h1>
+      <p className="card-front-name">Jane Appleseed</p>
+      <p className="card-front-date">00/00</p>
     </div>
   );
 }
